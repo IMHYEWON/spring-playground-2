b78ed0ab61a4139e09c02252b490bba7a5c5937b
IMHYEWON <hyewon1118@gmail.com>
2024-04-24 23:58:17 +0900

    Feat : Spring 2 버전으로 새 프로젝트 생성

---
+++ src/main/java/com/hyewon/springplayground2/SpringPlayground2Application.java
@@ -0,0 +1,13 @@
+package com.hyewon.springplayground2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlayground2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlayground2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyewon/springplayground2/SpringPlayground2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyewon.springplayground2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlayground2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
